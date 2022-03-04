da0e8796bd7ca211d82a37610635c778c57b3d34
Eraldo M R jr <rangelfisica@gmail.com>
2022-03-04 15:00:23 -0300

    update
    small fixes

--- modulo_scheduler/WorkVisualizer.hpp
+++ modulo_scheduler/WorkVisualizer.hpp
@@ -4,21 +4,29 @@
 #include <omp.h> 
 #include <algorithm>
 
+
+ 
+
+Uint32 process_w(Uint32 interval, void* uthis);
+ 
+
+
 class WorkVisualizer {
 private:
     static int count_windows;
     SDL_Window* window = NULL;
-    SDL_Renderer* gRenderer = NULL;
+    SDL_Renderer* gRenderer = NULL;    
 
     int* work_itens;  //-2 = not touched, -1 = touched, >=0 = touched and has a color
     size_t size_x;
     size_t size_y;
     int width, height;
 
+    
 
- WorkVisualizer(int width, int height):size_x(0),size_y(0),work_itens(nullptr),width(width),height(height){  
+ WorkVisualizer(int width, int height):quit(false), size_x(0),size_y(0),work_itens(nullptr),width(width),height(height){  
         if (WorkVisualizer::count_windows == 0) {
-            if (SDL_Init(SDL_INIT_VIDEO) < 0)
+            if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER ) < 0)
             {
                 printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
             }
@@ -27,6 +35,11 @@ private:
         this->window = SDL_CreateWindow("Work", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
         gRenderer = SDL_CreateRenderer(  this->window , -1, SDL_RENDERER_ACCELERATED );
 
+       Uint32 delay = (33 / 10) * 10;  /* To round it down to the nearest 10 ms */
+       SDL_TimerID my_timer_id = SDL_AddTimer(delay, process_w, this);
+
+
+
         WorkVisualizer::count_windows++;
     }
 
@@ -34,17 +47,18 @@ private:
         {
             double ko = (2.0E34) + k;
             while (ko > k) {
-                ko = (ko * 0.999995 - 1);
+                ko = (ko * 0.999995  - 1);
             }
         }
 
 public:
+   bool quit; 
   
     WorkVisualizer(int width, int height, int work_size):WorkVisualizer(width, height) { 
         work_itens = new int[work_size];
         std::fill( (work_itens),(work_itens)+work_size,-2);
         size_x = work_size;
-        size_y = 1 ;        
+        size_y = 1 ;                
     }
 
     WorkVisualizer(int width, int height, int work_size_x, int work_size_y) :WorkVisualizer(width, height) {
@@ -66,7 +80,11 @@ public:
     void reset() {
         std::fill((work_itens), (work_itens)+(size_x*size_y), -2);
     }
-    void update(int _i,int _j) {
+    void update(int i, int j){       
+               
+    }
+
+    void update( ) {
          
         //Clear screen
         SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
@@ -102,9 +120,7 @@ public:
 
         //Render work
         for (int i = 0; i < size_x; i++)  
-            for (int j = 0; j < size_y; j++) 
-        //int i = _i;
-        //int j = _j;
+            for (int j = 0; j < size_y; j++)  
         {
             fillRect.x = padding_x + i * (w + padding_x) + margim_x/2 ;
             fillRect.y = padding_y + j * (h + padding_y) + margim_y / 2;           
@@ -114,13 +130,14 @@ public:
             int ij = i + j * size_x;
 
             if (work_itens[ij] == -1) {
-                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x0, 0x0, 0xFF);                
+                SDL_SetRenderDrawColor(gRenderer, 0xFF,0x00,0x00 , 0xFF );
+                        
             }
             else if (work_itens[ij] == -2) {
                 SDL_SetRenderDrawColor(gRenderer, 0x0F, 0x0F, 0x0F, 0xFF );
             }
             else {
-                SDL_SetRenderDrawColor(gRenderer, 0xFF,0xFF - 20 * work_itens[ij],0x10 + 20* work_itens[ij] , 0xFF );
+                SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF - 20 * work_itens[ij],0x10 + 20* work_itens[ij] , 0xFF );
             }
 
             SDL_RenderFillRect(gRenderer, &fillRect);
@@ -129,7 +146,8 @@ public:
          SDL_RenderPresent(gRenderer);
 
     }
-    void touch(int i,int j = 0) {    
+    void touch(int i,int j = 0) {  
+        if (this->quit)   return;
         if ((i < 0) || (i >= size_x)) return;
         if ((j < 0) || (j >= size_y)) return;
         int ij = i + j * size_x;
@@ -150,5 +168,29 @@ public:
     
 };
 
+
+Uint32 process_w(Uint32 interval, void* uthis)
+{
+    SDL_Event e;
+    WorkVisualizer* wv = (WorkVisualizer*)uthis;
+     
+
+      while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        wv->quit = true;
+
+                    }
+                     if (e.type == SDL_KEYDOWN){
+                       //  wv->quit = true ;
+                     }
+                }
+ 
+ 
+      wv->update();
+      return interval;
+}
 
 int WorkVisualizer::count_windows = 0;
--- modulo_scheduler/exemplo_1.cpp
+++ modulo_scheduler/exemplo_1.cpp
@@ -2,24 +2,22 @@
 #include <omp.h>
  
 int main() {
-	const int works_x = 32;
-	const int works_y = 25;
+	const int works_x = 20;
+	const int works_y = 32;
 	WorkVisualizer wv(640, 640, works_x, works_y);
 
 loop_inicio:
 	wv.reset();
 
 	int i,j;
-#pragma omp parallel for  schedule(dynamic,1)  collapse(2)
+#pragma omp parallel for schedule(guided),  private(i)  
 	for ( j = 0; j < works_y; ++j)
-		for (i = 0; i < works_x; ++i)
-		
+		for (i = 0; i < works_x; ++i)		
 		{
-			wv.touch(i, j);
-		}
+	       wv.touch(i, j);			
+		}	
 	
-	
-	
+	if (wv.quit) return 0 ;
 	goto loop_inicio;
 	return 0;
 }
